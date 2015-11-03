124182ffe1162e640718debb489a5db6bcad7c54
RainsSoft <andyhebear@163.com>
2015-11-03 16:23:31 +0800

    add system.threading.dll with .net3.5 support .net4.0 task

---
+++ Bin/NET4/MonoTODOAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System {
+    public class MonoTODOAttribute:System.Attribute {
+        public string _desc;
+        public MonoTODOAttribute() { 
+        }
+        public MonoTODOAttribute(string desc) {
+            _desc = desc;
+        }
+    }
+}
